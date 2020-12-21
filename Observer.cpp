9d1e844ca6cff753066a65d3a099142000579b1c
“LMerCy” <jsluomc@163.com>
2020-12-22 00:27:19 +0800

    add statis displayer

--- Observer.cpp
+++ Observer.cpp
@@ -73,10 +73,34 @@ private:
     float temperature, humidity, pressure;
     SubjectInterface* subject;
 };
+class StatisticDisplayer : public ObserverInterface{
+public:
+    StatisticDisplayer(SubjectInterface* sub): maxtemp(-100.0), mintemp(200.0), avgtemp(0.0), numreadings(0){
+        sb = sub;
+        sub->registerObserver(this);
+    };
+    void update(float temp, float humi, float press){
+        avgtemp += temp;
+        maxtemp = maxtemp > temp ? maxtemp:temp;
+        mintemp = mintemp < temp ? mintemp:temp;
+        numreadings ++ ;
+        display();
+    }
+    void display(){
+        std::cout << "maxtemp: " << maxtemp << "mintemp: "<< mintemp << "avragetemp:"  << avgtemp / numreadings << std::endl;
+    }
+
+private:
+    float maxtemp=-100.0, mintemp=200.0, avgtemp=0.0;
+    int numreadings=0;
+    SubjectInterface *sb;
+};
 int main(){
     // test
     WeatherData wd;
     CurrentConditionsDisplayer *cur = new CurrentConditionsDisplayer(&wd);
+    StatisticDisplayer * statis = new StatisticDisplayer(&wd);
     wd.setMeasurements(3.0,2.0,4.0);
+    wd.setMeasurements(1.0,2.0,4.0);
     return 0;
 }
